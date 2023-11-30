eed509475de2ee8e57e9d0045a94baea3a8676d7
Dhruv Saxena <dhruv162002@gmail.com>
2023-11-30 12:18:51 +0530

    Complete board detection

---
+++ detect_board.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+import os
+
+def detect_chess_board(screen):
+    gray = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edges = cv2.Canny(blurred, 50, 150)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    chessboard_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 500]
+
+    if chessboard_contours:
+        chessboard = max(chessboard_contours, key=cv2.contourArea)
+        epsilon = 0.02 * cv2.arcLength(chessboard, True)
+        corners = cv2.approxPolyDP(chessboard, epsilon, True)
+        corners = corners.reshape(-1, 2)
+
+        if len(corners) != 4:
+            return None, None
+        
+        # Corners is a multi dimensional array of shape (N, 2), perform additional checks for lower latency
+        top_left = corners[0]
+        bottom_left = corners[1]
+        bottom_right = corners[2]
+        top_right = corners[3]
+
+        top_left_x, top_left_y = top_left
+        bottom_left_x, bottom_left_y = bottom_left
+        bottom_right_x, bottom_right_y = bottom_right
+        top_right_x, top_right_y = top_right
+
+        # Difference of x and y coordinates of the corners should be within 2 pixels
+        if abs(top_left_x - bottom_left_x) <= 2 and abs(top_right_x - bottom_right_x) <= 2 and abs(top_left_y - top_right_y) <= 2 and abs(bottom_left_y - bottom_right_y) <= 2:
+            if len(os.listdir('./processed')) == 0:
+                cv2.imwrite('./processed/gray.jpg', gray)
+                cv2.imwrite('./processed/blurred.jpg', blurred)
+                cv2.imwrite('./processed/edges.jpg', edges)
+
+            # Compute and verify board size to return it
+            board_width = abs(top_left_x - top_right_x)
+            board_height = abs(top_left_y - bottom_left_y) 
+            return corners, board_width
+        
+    return None, None
+
+while True:
+    screenshot = pyautogui.screenshot()
+    screen = np.array(screenshot)
+    screen = cv2.cvtColor(screen, cv2.COLOR_RGB2BGR)
+    chessboard_corners, board_size = detect_chess_board(screen)
+    if chessboard_corners is not None:
+        for i, corner in enumerate(chessboard_corners):
+            x, y = corner
+            print(f'Chess Board Corner {i + 1}: ({x}, {y})')
+        print(f'Board Size: {board_size}')
+    else:
+        print('Chess board not detected.')
+    time.sleep(1)
