6e0d521591db572ed807e1dd5f38882d73837cc3
Dhruv Saxena <dhruv162002@gmail.com>
2023-12-01 21:59:15 +0530

    Logic for piece identification

--- image_resizing.py
+++ image_resizing.py
@@ -3,16 +3,16 @@ import cv2
 
 board_size = 752
 desired_size = (int(board_size/8), int(board_size/8)) 
+subfolders = ["black_pieces", "white_pieces"]
 
-resized_folder_path = "./resized_pieces"
-os.makedirs(resized_folder_path, exist_ok=True)
-
-pieces_folder = "./pieces"
-for filename in os.listdir(pieces_folder):
-    if filename.endswith(".png") or filename.endswith(".jpg"):
-        piece_path = os.path.join(pieces_folder, filename)
-        piece_image = cv2.imread(piece_path, cv2.IMREAD_UNCHANGED)
-        resized_piece = cv2.resize(piece_image, desired_size)
-        resized_piece_path = os.path.join(resized_folder_path, filename)
-        cv2.imwrite(resized_piece_path, resized_piece)
-print("Resizing completed.")
+for subfolder in subfolders:
+    resized_folder_path = f"./resized_pieces/{subfolder}"
+    os.makedirs(resized_folder_path, exist_ok=True)
+    pieces_folder = f"./pieces/{subfolder}"
+    for filename in os.listdir(pieces_folder):
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            piece_path = os.path.join(pieces_folder, filename)
+            piece_image = cv2.imread(piece_path, cv2.IMREAD_UNCHANGED)
+            resized_piece = cv2.resize(piece_image, desired_size)
+            resized_piece_path = os.path.join(resized_folder_path, filename)
+            cv2.imwrite(resized_piece_path, resized_piece)
--- recognize_pieces.py
+++ recognize_pieces.py
@@ -9,6 +9,13 @@ template_size = template.shape[0]
 
 line_distance = template_size // 8
 
+subfolders = ["white_pieces", "black_pieces"]
+
+# 0 for white, 1 for black
+current_move = 1
+
+chessboard = [["." for i in range(8)] for j in range(8)]
+
 for i in range(8):
     for j in range(8):
         x1, y1 = j * line_distance, i * line_distance
@@ -16,32 +23,31 @@ for i in range(8):
 
         cell = template[y1:y2, x1:x2]
 
-        pieces_folder = "./resized_pieces"
+        pieces_folder = f"./resized_pieces/{subfolders[current_move]}"
+
+        # Maybe use a dictionary instead of two lists
+        piece_names = []
+        confidence_scores = []
+
         for piece_file in os.listdir(pieces_folder):
             if piece_file.endswith(".png"):
                 piece_path = os.path.join(pieces_folder, piece_file)
-                piece = cv2.imread(piece_path, cv2.IMREAD_UNCHANGED)
+                piece = cv2.imread(piece_path, cv2.IMREAD_UNCHANGED) 
                 
-                if piece.shape[2] == 4:
-                    piece = cv2.cvtColor(piece, cv2.COLOR_BGRA2BGR)
-
+                # Resize the piece image to match the cell size
                 piece = cv2.resize(piece, (cell.shape[1], cell.shape[0]))
 
                 result = cv2.matchTemplate(cell, piece, cv2.TM_CCOEFF_NORMED)
 
-                threshold = 0.8  
+                threshold = 0.53
 
                 locations = np.where(result >= threshold)
 
                 for loc in zip(*locations[::-1]):
-                    top_left = (x1 + loc[0], y1 + loc[1])
-                    bottom_right = (top_left[0] + piece.shape[1], top_left[1] + piece.shape[0])
-                    cv2.rectangle(template, top_left, bottom_right, (0, 0, 255), 2)
+                    piece_names.append(piece_file)
+                    confidence_scores.append(result[loc[1], loc[0]])
+                    break 
+        if len(confidence_scores) != 0:
+            final_piece_name = piece_names[confidence_scores.index(max(confidence_scores))]
+            print("Cell: ", i, j, "Piece: ", final_piece_name)
 
-                    # Print the position of the detected piece in the cell
-                    print(f"  Piece {piece_file}: ({top_left[0]}, {top_left[1]}) to ({bottom_right[0]}, {bottom_right[1]})")
-
-# Display the template image with bounding boxes
-cv2.imshow("Template with Bounding Boxes", template)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
